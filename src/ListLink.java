fdbf79170583ed47b7588a827bbd0858d47ccfe0
Ido Wigman <IdoWigman@gmail.com>
2026-05-23 23:32:37 +0300

    a

--- src/ListLink.java
+++ src/ListLink.java
@@ -14,7 +14,10 @@ public class ListLink<T> extends Element<T> {
      */
 	private ListLink<T> next;
 	private ListLink<T> prev;
-	private T value;
+	private int id = 0;
+	private int quality = 0;
+	private int price = 0;
+	private String name = "";
 	
 	public ListLink(int key, T satelliteData) {
 		super(key, satelliteData);
